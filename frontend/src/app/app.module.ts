9f20dd3759310dbac1d015378545a277e64db6f4
Olga Boris <olya_boris97@mail.ru>
2023-05-11 10:26:42 +0300

    reinitialized files

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoyoutComponent } from './shared/loyout/loyout.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoyoutComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
