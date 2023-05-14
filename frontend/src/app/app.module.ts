ce52ba91767d1ac2c26f5845ad7728bac1053a84
Olga Boris <olya_boris97@mail.ru>
2023-05-15 00:18:17 +0300

    add login component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { LayoutComponent } from './shared/layout/layout.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { MainComponent } from './views/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { MainComponent } from './views/main/main.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    MatSnackBarModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
